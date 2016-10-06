18ea94d466af3d2f534bf66bf19fa54479197e92
Dave Fancher <dave@davefancher.com>
2016-10-06 09:09:07 -0400

    Added main functionality

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ElevenNote.Data;
+using ElevenNote.Models;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        public static U CopyProperties<T, U>(T source)
+            where U : new()
+        {
+            var sourceType = typeof(T);
+            var targetType = typeof(U);
+
+            return
+                targetType
+                    .GetProperties()
+                    .Aggregate(
+                        new U(),
+                        (target, pi) =>
+                        {
+                            var value = sourceType.GetProperty(pi.Name)?.GetValue(source);
+                            if (value != null) pi.SetValue(target, value);
+                            return target;
+                        }
+                    );
+        }
+
+        private readonly Guid _userId;
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public IEnumerable<NoteListItemModel> GetNotes()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return
+                    context
+                        .Notes
+                        .Where(n => n.OwnerId == _userId)
+                        .Select(CopyProperties<NoteEntity, NoteListItemModel>)
+                        .ToArray();
+            }
+        }
+
+        private static NoteEntity GetNoteById(
+            ApplicationDbContext context,
+            int noteId,
+            Guid userId) =>
+                context
+                    .Notes
+                    .SingleOrDefault(n => n.NoteId == noteId && n.OwnerId == userId);
+
+        public NoteDetailModel GetNoteById(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var entity = GetNoteById(context, id, _userId);
+
+                return
+                    entity == null
+                    ? new NoteDetailModel()
+                    : CopyProperties<NoteEntity, NoteDetailModel>(entity);
+            }
+        }
+
+        public bool CreateNote(NoteCreateModel model)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var entity =
+                    CopyProperties<NoteCreateModel, NoteEntity>(model);
+                entity.OwnerId = _userId;
+                entity.CreatedUtc = DateTimeOffset.UtcNow;
+
+                context.Notes.Add(entity);
+
+                return context.SaveChanges() == 1;
+            }
+        }
+
+        public bool UpdateNote(NoteEditModel model)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var entity = GetNoteById(context, model.NoteId, _userId);
+
+                // TODO: Handle Note not found
+
+                entity.Title = model.Title;
+                entity.Content = model.Content;
+                entity.ModifiedUtc = DateTimeOffset.UtcNow;
+
+                return context.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteNote(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var entity = GetNoteById(context, id, _userId);
+
+                // TODO: Handle note not found
+
+                context.Notes.Remove(entity);
+
+                return context.SaveChanges() == 1;
+            }
+        }
+    }
+}
---
+++ ElevenNote/Controllers/NotesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+
+namespace ElevenNote.Controllers
+{
+    [Authorize]
+    public class NotesController : Controller
+    {
+        private readonly Lazy<NoteService> _svc;
+
+        public NotesController()
+        {
+            _svc =
+                new Lazy<NoteService>(
+                    () =>
+                    {
+                        var userId = Guid.Parse(User.Identity.GetUserId());
+                        return new NoteService(userId);
+                    }
+                );
+        }
+
+        public ActionResult Index()
+        {
+            var notes = _svc.Value.GetNotes();
+
+            return View(notes);
+        }
+
+        public ActionResult Create()
+        {
+            var model = new NoteCreateModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreateModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if(!_svc.Value.CreateNote(model))
+            {
+                ModelState.AddModelError("", "Unable to create note");
+                return View(model);
+            }
+
+            TempData["SaveResult"] = "Your note was created!";
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var note = _svc.Value.GetNoteById(id);
+
+            return View(note);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var details = _svc.Value.GetNoteById(id);
+            var note =
+                NoteService.CopyProperties<NoteDetailModel, NoteEditModel>(details);
+
+            return View(note);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEditModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (id != model.NoteId)
+            {
+                ModelState.AddModelError("", "C'mon man");
+                return View(model);
+            }
+
+            if(!_svc.Value.UpdateNote(model))
+            {
+                ModelState.AddModelError("", "Unable to update note");
+                return View(model);
+            }
+
+            TempData["SaveResult"] = "Your note was saved";
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public ActionResult DeleteGet(int id)
+        {
+            var detail = _svc.Value.GetNoteById(id);
+
+            return View(detail);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id, int noteId)
+        {
+            _svc.Value.DeleteNote(id);
+
+            TempData["SaveResult"] = "Your note was deleted";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
